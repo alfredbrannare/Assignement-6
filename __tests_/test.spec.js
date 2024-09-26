d088ef7bb279bed7c290b26fbc44d26b67f89310
Alfred <alfred-brannarer@hotmail.com>
2024-09-26 14:03:07 +0200

    first commit

---
+++ __tests_/test.spec.js
@@ -0,0 +1,9 @@
+const toUpperCase = require('/source/Skola/Assignement-6/main');
+
+test("Hopefully this prints it out toUpperCase", () => {
+
+    input = "hElLo WoRlD";
+    output = "HELLO WORLD";
+
+    expect(output).toBe("HELLO WORLD");
+});
---
+++ main.js
@@ -0,0 +1,6 @@
+function toUpperCase(test){
+    test.inneHTML = test.toUpperCase;
+    return;
+}
+
+module.exports = toUpperCase;
